137dfbcfdbb4de460acc84aa0dbc1da4c74a6796
AlanPinto21 <77893855+AlanPinto21@users.noreply.github.com>
2023-05-21 22:55:47 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+alert("Olá!");
+const alterarTema = document.getElementById("escurecer");
+
+const main = document.querySelector("main")
+console.log(body);
+
+const imagemTrocaDeIcone = document.querySelector(".imagem-botao");
+
+alterarTema.addEventListener("click", () => {
+    const modoEscuroAtivo = main.classList.contains("modo-escuro")
+
+    main.classList.toggle("modo-escuro") 
+
+    if (modoEscuroAtivo) {
+        imagemTrocaDeIcone.setAttribute('src', './src/imagens/imagens/moon.png');
+    } else {
+        imagemTrocaDeIcone.setAttribute('src', './src/imagens/imagens/sun.png')
+    }
+});
