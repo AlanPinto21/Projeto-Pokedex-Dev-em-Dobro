a1aef6a6596dd05ae73d541a705b3cc1616dc0e0
AlanPinto21 <77893855+AlanPinto21@users.noreply.github.com>
2023-05-23 14:17:44 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,19 @@
 alert("Olá!");
 const alterarTema = document.getElementById("escurecer");
 
-const main = document.querySelector("main")
+const main = document.querySelector("main");
 
 
 const imagemTrocaDeIcone = document.querySelector(".imagem-botao");
 
 alterarTema.addEventListener("click", () => {
-    const modoEscuroAtivo = main.classList.contains("modo-escuro")
+    const modoEscuroAtivo = main.classList.contains("modo-escuro");
 
-    main.classList.toggle("modo-escuro") 
+    main.classList.toggle("modo-escuro");
 
     if (modoEscuroAtivo) {
         imagemTrocaDeIcone.setAttribute('src', './src/imagens/imagens/moon.png');
     } else {
-        imagemTrocaDeIcone.setAttribute('src', './src/imagens/imagens/sun.png')
+        imagemTrocaDeIcone.setAttribute('src', './src/imagens/imagens/sun.png');
     }
 });
